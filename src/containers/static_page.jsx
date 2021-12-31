cc3c8bb91fab589b35146b7677a5041b6586750c
kiyomasa05 <m230549@gmail.com>
2021-12-31 18:42:10 +0900

    first

---
+++ src/containers/static_page.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+//component
+import { Header } from '../organism/Header/Header'
+import { Main } from '../component/Main'
+import { Footer } from '../component/Footer'
+
+export const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </Fragment>
+  )
+}
+
