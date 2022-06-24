3e79c68aa2b5454e174bbb463998a72e5bd783c3
kiyomasa05 <m230549@gmail.com>
2022-06-24 10:29:24 +0900

    404ページ作成

--- src/component/pages/404.jsx
+++ src/component/pages/404.jsx
@@ -1,5 +1,20 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 
-const Page404 = memo(() => <>404ページ</>);
+import { useHistory } from 'react-router-dom';
+import { Flex, Text, Button } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
+const Page404 = memo(() => {
+  const history = useHistory();
+  const onClickHome = useCallback(() => history.push('/'), [history]);
+  return (
+    <Flex alignItems="center" justifyContent="center" height="100vh">
+      <Text fontSize="6xl">404 not Found </Text>
+      <Button ml={10} rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline" onClick={onClickHome}>
+        Topへ戻る
+      </Button>
+    </Flex>
+  );
+});
 
 export default Page404;
